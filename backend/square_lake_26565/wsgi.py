2c32af67a676c9eed9d9f57652e6c6689ad41ea5
Crowdbotics <team@crowdbotics.com>
2021-05-11 18:26:10 +0000

    Initial commit.

---
+++ backend/square_lake_26565/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for square_lake_26565 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'square_lake_26565.settings')
+
+application = get_wsgi_application()
